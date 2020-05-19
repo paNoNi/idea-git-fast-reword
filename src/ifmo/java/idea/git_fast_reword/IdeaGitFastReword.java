6a6b9dbb8bbe287714b1d32c37155e4b23e781dc
paNoNi <novgorodcevnp@gmail.com>
2020-05-19 17:03:38 +0300

    Add basic functional

--- src/ifmo/java/idea/git_fast_reword/IdeaGitFastReword.java
+++ src/ifmo/java/idea/git_fast_reword/IdeaGitFastReword.java
@@ -1,43 +1,99 @@
 package ifmo.java.idea.git_fast_reword;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.AnActionEventVisitor;
+import com.intellij.ui.JBColor;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.util.ui.JBUI;
 import com.intellij.vcs.log.VcsFullCommitDetails;
-import com.intellij.vcs.log.VcsLog;
 import com.intellij.vcs.log.VcsLogDataKeys;
-import com.intellij.vcs.log.VcsLogDataProvider;
-import git4idea.GitUtil;
-import git4idea.repo.GitRemote;
-import git4idea.repo.GitRepository;
-import git4idea.repo.GitRepositoryFiles;
 import org.jetbrains.annotations.NotNull;
 import shchuko.git_fast_reword.GitFastReword;
-import git4idea.log.GitShowCommitInLogAction;
+import shchuko.git_fast_reword.GitOperationFailureException;
+import shchuko.git_fast_reword.RepositoryNotFoundException;
+import shchuko.git_fast_reword.RepositoryNotOpenedException;
 
-import static git4idea.GitUtil.getRepositoryManager;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
 
 public class IdeaGitFastReword extends AnAction {
+    ideaGitFastRewordGUI gitFastRewordGUI;
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         try(GitFastReword gitFastReword = new GitFastReword()) {
 
-            Project project = anActionEvent.getRequiredData(CommonDataKeys.PROJECT);
-            VcsLogDataProvider data = anActionEvent.getRequiredData(VcsLogDataKeys.VCS_LOG_DATA_PROVIDER);
-            VcsLog log = anActionEvent.getRequiredData(VcsLogDataKeys.VCS_LOG);
-            final VcsFullCommitDetails commit = anActionEvent.getRequiredData(VcsLogDataKeys.VCS_LOG).getSelectedDetails().get(0);
-            //final GitRepository gitRepository = getRepositoryManager(project).getRepositoryForRoot(commit.getRoot());
-            //commit.getId().asString()
-            /*Notification notification = new Notification("fast-reword", "Fast-reword", commit.getRoot().getPath(),
-                    NotificationType.INFORMATION);
-            Notifications.Bus.notify(notification);
-*/
+            final VcsFullCommitDetails commit =
+                    anActionEvent.getRequiredData(VcsLogDataKeys.VCS_LOG).getSelectedDetails().get(0);
 
+            gitFastRewordGUI = new ideaGitFastRewordGUI(commit);
+            pressCancel();
+            pressOk(gitFastReword, commit);
+            listenButtonOk();
 
         }
+
     }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+
+        final VcsFullCommitDetails commit = e.getRequiredData(VcsLogDataKeys.VCS_LOG).getSelectedDetails().get(0);
+
+        e.getPresentation().setEnabled(commit.getParents().size() == 1);
+    }
+
+    private void listenButtonOk() {
+        gitFastRewordGUI.getText().addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+                gitFastRewordGUI.getButtonOk().setEnabled(!gitFastRewordGUI.getText().getText().isEmpty());
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                gitFastRewordGUI.getButtonOk().setEnabled(!gitFastRewordGUI.getText().getText().isEmpty());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                gitFastRewordGUI.getButtonOk().setEnabled(!gitFastRewordGUI.getText().getText().isEmpty());
+            }
+
+        });
+    }
+
+    private void pressOk(GitFastReword gitFastReword, VcsFullCommitDetails commit) {
+        gitFastRewordGUI.getButtonOk().addActionListener(actionEvent -> {
+            try {
+                gitFastReword.openRepository(Paths.get(commit.getRoot().getPath()));
+                gitFastReword.reword(commit.getId().asString(), gitFastRewordGUI.getText().getText());
+                gitFastRewordGUI.getJFrame().dispose();
+            } catch (RepositoryNotFoundException | IOException | GitOperationFailureException | RepositoryNotOpenedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void pressCancel() {
+        JFrame jFrame = gitFastRewordGUI.getJFrame();
+        gitFastRewordGUI.getButtonCancel().addActionListener(actionEvent -> {
+            if (actionEvent.getActionCommand().equals("Cancel")) {
+                jFrame.dispose();
+            }
+        });
+    }
+
+
 }
---
+++ src/ifmo/java/idea/git_fast_reword/ideaGitFastRewordGUI.java
@@ -0,0 +1,119 @@
+package ifmo.java.idea.git_fast_reword;
+
+import com.intellij.ui.JBColor;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.util.ui.JBUI;
+import com.intellij.vcs.log.VcsFullCommitDetails;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
+
+public class ideaGitFastRewordGUI {
+    private JButton buttonOk;
+    private JButton buttonCancel;
+    private JFrame jFrame;
+    private JPanel jPanel;
+    private JLabel jLabel;
+    private JTextArea jTextArea;
+
+    public ideaGitFastRewordGUI(VcsFullCommitDetails commit){
+        setJFrame();
+        setJPanel();
+        setButtonOK(jPanel);
+        setButtonCancel(jPanel);
+        setLabel(jPanel, commit);
+        setText(jPanel, commit);
+        jFrame.add(jPanel);
+    }
+
+
+    private void setJFrame() {
+        jFrame = new JFrame("Fast Edit Commit Message");
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        int width = dimension.width / 6;
+        int height = dimension.height / 6;
+        jFrame.setSize(width, height);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+
+    private void setJPanel() {
+        jPanel = new JPanel();
+        jPanel.setLayout(new GridBagLayout());
+    }
+
+    private void setText(JPanel panel, VcsFullCommitDetails commit) {
+        jTextArea = new JTextArea(commit.getFullMessage());
+        jTextArea.setLineWrap(true);
+        GridBagConstraints constraints = getContain(0.5f, 1, 2, 2, 10, 5);
+        jTextArea.setBorder(JBUI.Borders.empty(5, 10));
+        constraints.fill = GridBagConstraints.BOTH;
+        JScrollPane jScroll = new JBScrollPane(jTextArea, VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(jScroll, constraints);
+    }
+
+
+    private void setLabel(JPanel panel, VcsFullCommitDetails commit) {
+        final String text ="Edit message for commit " + commit.getId().toShortString() + " by "
+                + commit.getAuthor().getName();
+        jLabel = new JLabel(text);
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(jLabel, getContain(0, 0, 1, 1, 12, 1));
+    }
+
+
+    private void setButtonOK(JPanel panel) {
+        buttonOk = new JButton("OK");
+        panel.add(buttonOk, getContain(0.5f, 0, 8, 6, 2, 1));
+    }
+
+    private void setButtonCancel(JPanel panel) {
+        buttonCancel = new JButton("Cancel");
+        buttonCancel.setName("Cancel");
+        panel.add(buttonCancel, getContain(0.5f, 0, 8, 10, 2, 1));
+    }
+
+    private GridBagConstraints getContain(float weightx, float weighty, int gridy, int gridx, int gridwidth, int gridheight) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.weightx = weightx;
+        constraints.weighty = weighty;
+        constraints.gridy = gridy;
+        constraints.gridx = gridx;
+        constraints.gridwidth = gridwidth;
+        constraints.gridheight = gridheight;
+        return constraints;
+    }
+
+    public JFrame getJFrame() {
+        return jFrame;
+    }
+
+    public JPanel getjPanel() {
+        return jPanel;
+    }
+
+    public JButton getButtonOk() {
+        return buttonOk;
+    }
+
+    public JButton getButtonCancel() {
+        return buttonCancel;
+    }
+
+    public JLabel getjLabel() {
+        return jLabel;
+    }
+
+    public JTextArea getText() {
+        return jTextArea;
+    }
+
+}
